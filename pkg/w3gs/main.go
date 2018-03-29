b4dfca1c4db2450497a2fb01d9b1611c47b0e7e8
NielsAD <niels.a.d@gmail.com>
2018-03-29 08:15:10 +0200

    w3gs: Implemented structure and binary (de)serialization for most known packets

---
+++ pkg/w3gs/main.go
@@ -0,0 +1,262 @@
+// Package w3gs implements the game protocol for Warcraft III.
+//
+// Based on the protocol documentation from https://bnetdocs.org/
+//
+// General format:
+//
+//    (UINT8)  Protocol signature (0xF7)
+//    (UINT8)  Packet type ID
+//    (UINT16) Packet size
+//    [Packet Data]
+//
+package w3gs
+
+import (
+	"encoding"
+	"encoding/binary"
+)
+
+// Packet interface.
+type Packet interface {
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
+}
+
+// W3GS magic number
+const protocolsig = 0xF7
+
+// AF_INET
+const connAddressFamily uint16 = 2
+
+// W3GS packet type identifiers
+const (
+	pidUnknownPacket     = 0x00
+	pidPingFromHost      = 0x01
+	pidSlotInfoJoin      = 0x04
+	pidRejectJoin        = 0x05
+	pidPlayerInfo        = 0x06
+	pidPlayerLeft        = 0x07
+	pidPlayerLoaded      = 0x08
+	pidSlotInfo          = 0x09
+	pidCountDownStart    = 0x0A
+	pidCountDownEnd      = 0x0B
+	pidIncomingAction    = 0x0C
+	pidChatFromHost      = 0x0F
+	pidStartLag          = 0x10
+	pidStopLag           = 0x11
+	pidLeaveAck          = 0x1B
+	pidReqJoin           = 0x1E
+	pidLeaveReq          = 0x21
+	pidGameLoadedSelf    = 0x23
+	pidOutgoingAction    = 0x26
+	pidOutgoingKeepAlive = 0x27
+	pidChatToHost        = 0x28
+	pidDropReq           = 0x29
+	pidSearchGame        = 0x2F
+	pidGameInfo          = 0x30
+	pidCreateGame        = 0x31
+	pidRefreshGame       = 0x32
+	pidDecreateGame      = 0x33
+	pidPingFromOthers    = 0x35
+	pidPongToOthers      = 0x36
+	pidClientInfo        = 0x37
+	pidMapCheck          = 0x3D
+	pidStartDownload     = 0x3F
+	pidMapSize           = 0x42
+	pidMapPart           = 0x43
+	pidMapPartOK         = 0x44
+	pidMapPartError      = 0x45
+	pidPongToHost        = 0x46
+	pidIncomingAction2   = 0x48
+)
+
+//pidPlayerKicked = 0x1c
+//pidChatOthers   = 0x34
+
+// Game versions
+var (
+	gameWAR3 = "WAR3"
+	gameW3XP = "W3XP"
+)
+
+// Slot layout
+const (
+	LayoutMelee               = 0x00
+	LayoutCustomForces        = 0x01
+	LayoutFixedPlayerSettings = 0x02
+)
+
+// Slot status
+const (
+	SlotOpen     = 0x00
+	SlotClosed   = 0x01
+	SlotOccupied = 0x02
+)
+
+// Race preference
+const (
+	RaceHuman      = 0x01
+	RaceOrc        = 0x02
+	RaceNightElf   = 0x04
+	RaceUndead     = 0x08
+	RaceDemon      = 0x10
+	RaceRandom     = 0x20
+	RaceSelectable = 0x40
+)
+
+// AI difficulty
+const (
+	ComputerEasy   = 0x00
+	ComputerNormal = 0x01
+	ComputerInsane = 0x02
+)
+
+// RejectJoin reason
+const (
+	RejectJoinInvalid   = 0x07
+	RejectJoinFull      = 0x09
+	RejectJoinStarted   = 0x0A
+	RejectJoinWrongPass = 0x1B
+)
+
+// PlayerLeft reason
+const (
+	LeaveDisconnect    = 0x01
+	LeaveLost          = 0x07
+	LeaveLostBuildings = 0x08
+	LeaveWon           = 0x09
+	LeaveDraw          = 0x0A
+	LabeObserver       = 0x0B
+	LeaveLobby         = 0x0D
+)
+
+// Chat type
+const (
+	ChatMessage        = 0x10
+	ChatTeamChange     = 0x11
+	ChatColorChange    = 0x12
+	ChatRaceChange     = 0x13
+	ChatHandicapChange = 0x14
+	ChatMessageExtra   = 0x20
+)
+
+// Game type flags
+const (
+	GameTypeNewGame        = 0x000001
+	GameTypeBlizzardSigned = 0x000008
+	GameTypeLadder         = 0x000020
+	GameTypeSavedGame      = 0x000200
+	GameTypePrivateGame    = 0x000800
+	GameTypeUserMap        = 0x002000
+	GameTypeBlizzardMap    = 0x004000
+	GameTypeTypeMelee      = 0x008000
+	GameTypeTypeScenario   = 0x010000
+	GameTypeSizeSmall      = 0x020000
+	GameTypeSizeMedium     = 0x040000
+	GameTypeSizeLarge      = 0x080000
+	GameTypeObsFull        = 0x100000
+	GameTypeObsOnDeath     = 0x200000
+	GameTypeObsNone        = 0x400000
+
+	GameTypeMaskObs   = GameTypeObsFull | GameTypeObsFull | GameTypeObsNone
+	GameTypeMaskMaker = GameTypeUserMap | GameTypeBlizzardMap
+	GameTypeMaskSize  = GameTypeSizeSmall | GameTypeSizeMedium | GameTypeSizeLarge
+)
+
+// UnmarshalPacket decodes binary data and returns it in the proper (unmarshalled) packet type.
+func UnmarshalPacket(data []byte) (Packet, int, error) {
+	if len(data) < 4 || (data)[0] != protocolsig {
+		return nil, 0, errMalformedData
+	}
+	var size = int(binary.LittleEndian.Uint16(data[2:]))
+	if size > len(data) {
+		return nil, 0, errMalformedData
+	}
+
+	var pkt Packet
+
+	switch data[1] {
+	case pidPingFromHost:
+		pkt = &PingFromHost{}
+	case pidSlotInfoJoin:
+		pkt = &SlotInfoJoin{}
+	case pidRejectJoin:
+		pkt = &RejectJoin{}
+	case pidPlayerInfo:
+		pkt = &PlayerInfo{}
+	case pidPlayerLeft:
+		pkt = &PlayerLeft{}
+	case pidPlayerLoaded:
+		pkt = &PlayerLoaded{}
+	case pidSlotInfo:
+		pkt = &SlotInfo{}
+	case pidCountDownStart:
+		pkt = &CountDownStart{}
+	case pidCountDownEnd:
+		pkt = &CountDownEnd{}
+	case pidIncomingAction:
+		pkt = &IncomingAction{}
+	case pidChatFromHost:
+		pkt = &ChatFromHost{}
+	case pidStartLag:
+		pkt = &StartLag{}
+	case pidStopLag:
+		pkt = &StopLag{}
+	case pidLeaveAck:
+		pkt = &LeaveAck{}
+	case pidReqJoin:
+		pkt = &ReqJoin{}
+	case pidLeaveReq:
+		pkt = &LeaveReq{}
+	case pidGameLoadedSelf:
+		pkt = &GameLoadedSelf{}
+	case pidOutgoingAction:
+		pkt = &OutgoingAction{}
+	case pidOutgoingKeepAlive:
+		pkt = &OutgoingKeepAlive{}
+	case pidChatToHost:
+		pkt = &ChatToHost{}
+	case pidDropReq:
+		pkt = &DropReq{}
+	case pidSearchGame:
+		pkt = &SearchGame{}
+	case pidGameInfo:
+		pkt = &GameInfo{}
+	case pidCreateGame:
+		pkt = &CreateGame{}
+	case pidRefreshGame:
+		pkt = &RefreshGame{}
+	case pidDecreateGame:
+		pkt = &DecreateGame{}
+	case pidPingFromOthers:
+		pkt = &PingFromOthers{}
+	case pidPongToOthers:
+		pkt = &PongToOthers{}
+	case pidClientInfo:
+		pkt = &ClientInfo{}
+	case pidMapCheck:
+		pkt = &MapCheck{}
+	case pidStartDownload:
+		pkt = &StartDownload{}
+	case pidMapSize:
+		pkt = &MapSize{}
+	case pidMapPart:
+		pkt = &MapPart{}
+	case pidMapPartOK:
+		pkt = &MapPartOK{}
+	case pidMapPartError:
+		pkt = &MapPartError{}
+	case pidPongToHost:
+		pkt = &PongToHost{}
+	case pidIncomingAction2:
+		pkt = &IncomingAction{}
+	default:
+		pkt = &UnknownPacket{}
+	}
+
+	if err := pkt.UnmarshalBinary(data[:size]); err != nil {
+		return nil, size, err
+	}
+
+	return pkt, size, nil
+}
---
+++ pkg/w3gs/packets_test.go
@@ -0,0 +1,327 @@
+package w3gs
+
+import (
+	"math/rand"
+	"net"
+	"reflect"
+	"testing"
+)
+
+func TestMarshalPacket(t *testing.T) {
+	var types = []Packet{
+		&UnknownPacket{
+			Blob: []byte{protocolsig, 255, 4, 0},
+		},
+		&PingFromHost{},
+		&PingFromHost{
+			Ping: 444,
+		},
+		&SlotInfoJoin{},
+		&SlotInfoJoin{
+			Slots: []SlotData{SlotData{
+				1, 2, 3, true, 5, 6, 7, 8, 9,
+			}},
+			RandomSeed:   10,
+			SlotLayout:   LayoutMelee,
+			NumPlayers:   12,
+			PlayerID:     13,
+			ExternalPort: 14,
+			ExternalIP:   net.IP{15, 16, 17, 18},
+		},
+		&RejectJoin{},
+		&RejectJoin{
+			Reason: LeaveWon,
+		},
+		&PlayerInfo{},
+		&PlayerInfo{
+			JoinCounter:  1,
+			PlayerID:     2,
+			PlayerName:   "Moon",
+			ExternalPort: 4,
+			ExternalIP:   net.IP{5, 6, 7, 8},
+			InternalPort: 9,
+			InternalIP:   net.IP{10, 11, 12, 13},
+		},
+		&PlayerLeft{},
+		&PlayerLeft{
+			PlayerID: 1,
+			Reason:   LeaveLost,
+		},
+		&PlayerLoaded{},
+		&PlayerLoaded{
+			PlayerID: 123,
+		},
+		&SlotInfo{},
+		&SlotInfo{
+			Slots: []SlotData{SlotData{
+				1, 2, 3, true, 5, 6, 7, 8, 9,
+			}},
+		},
+		&CountDownStart{},
+		&CountDownEnd{},
+		&IncomingAction{},
+		&IncomingAction{
+			Fragment:     false,
+			SendInterval: 50,
+			Actions: []PlayerAction{
+				PlayerAction{1, make([]byte, 13)},
+				PlayerAction{11, make([]byte, 13)},
+			},
+		},
+		&IncomingAction{
+			Fragment:     true,
+			SendInterval: 50,
+			Actions: []PlayerAction{
+				PlayerAction{1, make([]byte, 13)},
+				PlayerAction{11, make([]byte, 13)},
+			},
+		},
+		&ChatFromHost{},
+		&ChatFromHost{
+			RecipientIDs: []uint8{1, 2, 3},
+			SenderID:     4,
+			Flags:        ChatMessage,
+			ExtraFlags:   5,
+			Message:      "I come from the darkness of the pit",
+		},
+		&StartLag{},
+		&StartLag{
+			Players: []LagPlayer{
+				LagPlayer{1, 2},
+				LagPlayer{3, 4},
+				LagPlayer{5, 6},
+			},
+		},
+		&StopLag{},
+		&StopLag{
+			LagPlayer{1, 2},
+		},
+		&LeaveAck{},
+		&ReqJoin{},
+		&ReqJoin{
+			HostCounter:  1,
+			EntryKey:     2,
+			ListenPort:   3,
+			PeerKey:      4,
+			PlayerName:   "Grubby",
+			InternalPort: 6,
+			InternalIP:   net.IP{7, 8, 9, 10},
+		},
+		&LeaveReq{},
+		&LeaveReq{
+			Reason: LeaveLost,
+		},
+		&GameLoadedSelf{},
+		&OutgoingAction{},
+		&OutgoingAction{
+			[]byte{2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		&OutgoingKeepAlive{},
+		&OutgoingKeepAlive{
+			Checksum: 456,
+		},
+		&ChatToHost{},
+		&ChatToHost{
+			Messages: []ChatToHostMessage{
+				ChatToHostMessage{
+					RecipientID: 1,
+					SenderID:    2,
+					Flags:       ChatMessage,
+					Message:     "What a foolish boy!",
+				},
+				ChatToHostMessage{
+					RecipientID: 3,
+					SenderID:    4,
+					Flags:       ChatColorChange,
+					NewVal:      5,
+				},
+			},
+		},
+		&DropReq{},
+		&SearchGame{},
+		&SearchGame{
+			TFT:     true,
+			Version: 666,
+		},
+		&GameInfo{},
+		&GameInfo{
+			TFT:               true,
+			HostCounter:       1,
+			PlayersInGame:     2,
+			GameName:          "game1",
+			StatString:        "xxxxx",
+			SlotsTotal:        24,
+			GameTypeFlags:     GameTypeNewGame,
+			SlotsAvailable:    22,
+			TimeSinceCreation: 8,
+			GamePort:          9,
+		},
+		&CreateGame{},
+		&CreateGame{
+			TFT:           true,
+			HostCounter:   2,
+			PlayersInGame: 3,
+		},
+		&RefreshGame{},
+		&RefreshGame{
+			HostCounter:    1,
+			PlayersInGame:  2,
+			SlotsAvailable: 3,
+		},
+		&DecreateGame{},
+		&DecreateGame{
+			HostCounter: 777,
+		},
+		&PingFromOthers{},
+		&PingFromOthers{
+			Ping: 123,
+		},
+		&PongToOthers{},
+		&PongToOthers{
+			Pong: 456,
+		},
+		&ClientInfo{},
+		&ClientInfo{
+			JoinCounter: 1,
+			PlayerID:    2,
+		},
+		&MapCheck{},
+		&MapCheck{
+			FilePath:          "/beans.w3x",
+			FileSize:          2,
+			MapInfo:           3,
+			FileCrcEncryption: 4,
+			FileSha1Hash:      5,
+		},
+		&StartDownload{},
+		&StartDownload{
+			PlayerID: 111,
+		},
+		&MapSize{},
+		&MapSize{
+			Ready:   true,
+			MapSize: 2,
+		},
+		&MapPart{},
+		&MapPart{
+			RecipientID:         1,
+			SenderID:            2,
+			ChunkPositionInFile: 3,
+			Data:                []byte{5, 6, 7, 8, 9},
+		},
+		&MapPartOK{},
+		&MapPartOK{
+			RecipientID:         1,
+			SenderID:            2,
+			ChunkPositionInFile: 3,
+		},
+		&MapPartError{},
+		&PongToHost{},
+		&PongToHost{
+			Pong: 999,
+		},
+	}
+
+	var tooShort = make([]byte, 0)
+	var tooLong = make([]byte, 2048)
+
+	for _, pkt := range types {
+		var data, err = pkt.MarshalBinary()
+		if err != nil {
+			t.Log(reflect.TypeOf(pkt))
+			t.Fatal(err)
+		}
+
+		if len(data) != cap(data) {
+			t.Logf("Capacity mismatch for %v (%v != %v)", reflect.TypeOf(pkt), len(data), cap(data))
+		}
+
+		var pkt2, size, e = UnmarshalPacket(data)
+		if e != nil {
+			t.Log(reflect.TypeOf(pkt))
+			t.Fatal(e)
+		}
+		if size != len(data) {
+			t.Fatalf("UnmarshalPacket size mismatch for %v", reflect.TypeOf(pkt))
+		}
+		if reflect.TypeOf(pkt2) != reflect.TypeOf(pkt) {
+			t.Fatalf("UnmarshalPacket type mismatch %v != %v", reflect.TypeOf(pkt2), reflect.TypeOf(pkt))
+		}
+		if !reflect.DeepEqual(pkt, pkt2) {
+			t.Logf("I: %+v", pkt)
+			t.Logf("O: %+v", pkt2)
+			t.Errorf("UnmarshalPacket value mismatch for %v", reflect.TypeOf(pkt))
+		}
+
+		err = pkt.UnmarshalBinary(tooShort)
+		if err != errMalformedData {
+			t.Fatalf("errMalformedData expected for %v", reflect.TypeOf(pkt))
+		}
+
+		err = pkt.UnmarshalBinary(tooLong)
+		if _, ok := pkt.(*UnknownPacket); err != errMalformedData && err != errInvalidChecksum && !ok {
+			t.Fatalf("errMalformedData expected for %v", reflect.TypeOf(pkt))
+		}
+	}
+}
+
+func TestUnmarshalPacket(t *testing.T) {
+	if _, _, e := UnmarshalPacket([]byte{protocolsig, 255}); e != errMalformedData {
+		t.Fatal("errMalformedData expected if no size")
+	}
+	if _, _, e := UnmarshalPacket([]byte{protocolsig, 255, 255, 0}); e != errMalformedData {
+		t.Fatal("errMalformedData expected if invalid size")
+	}
+}
+
+func BenchmarkMarshalBinary(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var pkt = SlotInfoJoin{
+			Slots: []SlotData{
+				SlotData{0, 0, 0, false, 0, 0, 0, 0, 0},
+				SlotData{1, 0, 0, true, 1, 1, 0, 0, 0},
+				SlotData{2, 0, 0, false, 2, 2, 0, 0, 0},
+				SlotData{3, 0, 0, true, 3, 3, 0, 0, 0},
+				SlotData{4, 0, 0, false, 4, 4, 0, 0, 0},
+				SlotData{5, 0, 0, true, 5, 5, 0, 0, 0},
+				SlotData{6, 0, 0, false, 6, 6, 0, 0, 0},
+				SlotData{7, 0, 0, true, 7, 7, 0, 0, 0},
+			},
+			RandomSeed:   rand.Uint32(),
+			SlotLayout:   LayoutMelee,
+			NumPlayers:   uint8(rand.Intn(24)),
+			PlayerID:     uint8(rand.Intn(2552)),
+			ExternalPort: uint16(rand.Intn(65534)),
+			ExternalIP:   net.IPv4bcast,
+		}
+		pkt.MarshalBinary()
+	}
+}
+
+func BenchmarkUnmarshalBinary(b *testing.B) {
+	var pkt = SlotInfoJoin{
+		Slots: []SlotData{
+			SlotData{0, 0, 0, false, 0, 0, 0, 0, 0},
+			SlotData{1, 0, 0, true, 1, 1, 0, 0, 0},
+			SlotData{2, 0, 0, false, 2, 2, 0, 0, 0},
+			SlotData{3, 0, 0, true, 3, 3, 0, 0, 0},
+			SlotData{4, 0, 0, false, 4, 4, 0, 0, 0},
+			SlotData{5, 0, 0, true, 5, 5, 0, 0, 0},
+			SlotData{6, 0, 0, false, 6, 6, 0, 0, 0},
+			SlotData{7, 0, 0, true, 7, 7, 0, 0, 0},
+		},
+		RandomSeed:   rand.Uint32(),
+		SlotLayout:   LayoutMelee,
+		NumPlayers:   uint8(rand.Intn(24)),
+		PlayerID:     uint8(rand.Intn(2552)),
+		ExternalPort: uint16(rand.Intn(65534)),
+		ExternalIP:   net.IPv4bcast,
+	}
+	var data, _ = pkt.MarshalBinary()
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		var res SlotInfoJoin
+		res.UnmarshalBinary(data)
+	}
+}
