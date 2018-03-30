cc0a444c689253f79706bd961033e6d4f79aabb0
NielsAD <niels.a.d@gmail.com>
2018-03-30 11:36:30 +0200

    w3gsdump: Sniff on all ports and limit the size of blobs in text dumps

--- cmd/w3gsdump/main.go
+++ cmd/w3gsdump/main.go
@@ -4,7 +4,6 @@ package main
 import (
 	"encoding/hex"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"reflect"
@@ -19,8 +18,8 @@ var (
 	fname   = flag.String("f", "", "Filename to read from")
 	iface   = flag.String("i", "any", "Interface to read packets from")
 	promisc = flag.Bool("promisc", true, "Set promiscuous mode")
-	snaplen = flag.Int("s", 65536, "Snap length (number of bytes max to read per packet")
-	port    = flag.Int("p", 6112, "Set port to filter")
+	snaplen = flag.Int("s", 65536, "Snap length (max number of bytes to read per packet")
+	bloblen = flag.Int("b", 128, "Max number of bytes to print per blob ")
 )
 
 func main() {
@@ -30,7 +29,7 @@ func main() {
 	var err error
 	if *fname != "" {
 		if handle, err = pcap.OpenOffline(*fname); err != nil {
-			log.Fatal("PCAP OpenOffline error:", err)
+			log.Fatal("Could not open pcap file:", err)
 		}
 	} else {
 		handle, err = pcap.OpenLive(*iface, int32(*snaplen), *promisc, pcap.BlockForever)
@@ -40,38 +39,52 @@ func main() {
 		defer handle.Close()
 	}
 
-	if err = handle.SetBPFFilter(fmt.Sprintf("port %v and (tcp or udp)", *port)); err != nil {
+	if err = handle.SetBPFFilter("tcp or udp"); err != nil {
 		log.Fatal("BPF filter error:", err)
 	}
 
 	var logger = log.New(os.Stdout, "", log.Ltime)
 
 	var src = gopacket.NewPacketSource(handle, handle.LinkType())
-	var in = src.Packets()
-	for {
-		select {
-		case packet := <-in:
-			var app = packet.ApplicationLayer()
-			if app == nil {
-				continue
+	for packet := range src.Packets() {
+		var app = packet.ApplicationLayer()
+		if app == nil {
+			continue
+		}
+
+		var payload = app.Payload()
+		for len(payload) > 0 && payload[0] == w3gs.ProtocolSig {
+			var w3p, size, err = w3gs.UnmarshalPacket(payload)
+			if err != nil {
+				logger.Printf("[%-3v] %-32v %-14v %v\n", packet.TransportLayer().LayerType(), packet.NetworkLayer().NetworkFlow(), "ERROR", err)
+				logger.Printf("Payload:\n%v", hex.Dump(payload))
+				break
 			}
 
-			var payload = app.Payload()
-			for len(payload) > 0 {
-				var w3p, size, err = w3gs.UnmarshalPacket(payload)
-				if err != nil {
-					if size > 0 {
-						payload = payload[:size]
+			// Truncate blobs
+			switch p := w3p.(type) {
+			case *w3gs.UnknownPacket:
+				p.Blob = p.Blob[:*bloblen]
+			case *w3gs.GameAction:
+				p.Data = p.Data[:*bloblen]
+			case *w3gs.TimeSlot:
+				var blobsize = 0
+				for i := 0; i < len(p.Actions); i++ {
+					if blobsize+len(p.Actions[i].Data) > *bloblen {
+						p.Actions[i].Data = p.Actions[i].Data[:*bloblen-blobsize]
 					}
-
-					logger.Printf("[%-3v] %-32v %-17v %v\n", packet.TransportLayer().LayerType(), packet.NetworkLayer().NetworkFlow(), "ERROR", err)
-					logger.Printf("Payload:\n%v", hex.Dump(payload))
-					break
+					blobsize += len(p.Actions[i].Data)
+					if blobsize >= *bloblen {
+						p.Actions = p.Actions[:i+1]
+						break
+					}
 				}
-
-				payload = payload[size:]
-				logger.Printf("[%-3v] %-32v %-17v %+v\n", packet.TransportLayer().LayerType(), packet.NetworkLayer().NetworkFlow(), reflect.TypeOf(w3p).String()[6:], w3p)
+			case *w3gs.MapPart:
+				p.Data = p.Data[:*bloblen]
 			}
+
+			payload = payload[size:]
+			logger.Printf("[%-3v] %-32v %-14v %+v\n", packet.TransportLayer().LayerType(), packet.NetworkLayer().NetworkFlow(), reflect.TypeOf(w3p).String()[6:], w3p)
 		}
 	}
 }
